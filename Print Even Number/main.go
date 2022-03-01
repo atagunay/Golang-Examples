e97695d095023d0ad578faef79e882e027b96b03
atagunay <180704020@st.maltepe.edu.tr>
2022-03-01 23:28:19 +0300

    find even numbers

---
+++ Print Even Number/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	// User inform about enter the number
+	fmt.Println("Please enter a number")
+
+	reader := bufio.NewReader(os.Stdin) // create reader
+	input, _ := reader.ReadString('\n') // take strings
+
+	input = strings.TrimSpace(input)       // remove space begin and end of the string
+	inputNumeric, _ := strconv.Atoi(input) // strings convert to int
+
+	for i := 0; i < inputNumeric; i++ { // for loop
+		if i%2 == 0 { // control even or odd
+			fmt.Println(i) // if i is even, print
+		}
+	}
+
+}
