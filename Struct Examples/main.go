70caba209fe173b07db23bc23e83a6b5b21c4e4d
Ata Günay <180704020@st.maltepe.edu.tr>
2022-03-16 23:07:50 +0300

    added struct functions

--- Struct Examples/main.go
+++ Struct Examples/main.go
@@ -2,17 +2,30 @@ package main
 
 import "fmt"
 
+type product struct {
+	id   int
+	name string
+}
+
 func main() {
-	type product struct {
-		id   int
-		name string
-	}
 
 	var temp product
 	temp = product{1, "computer"}
 	fmt.Println(temp)
+	save(temp)
+	temp.save2()
 
 	temp2 := &temp
 	fmt.Println(*temp2)
+	temp2.save2()
+	save(*temp2)
 
+}
+
+func save(p product) {
+	fmt.Println("added with save")
+}
+
+func (p product) save2() {
+	fmt.Println("added with save2")
 }
