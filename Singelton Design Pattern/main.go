ff7fef0f438883aeb25e28490ef6e994f693fdf0
atagunay <atafb2000gunay@gmail.com>
2023-09-25 23:21:06 +0100

    [improvement] Add mutex

--- Singelton Design Pattern/main.go
+++ Singelton Design Pattern/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // Student /* Singleton object
 type Student struct {
@@ -9,14 +12,22 @@ type Student struct {
 
 // Variable for keeping single object's reference
 var student *Student
+var lock = &sync.Mutex{}
 
 // Constructor method
 // Return address of student
 func getNewStudent() *Student {
 	if student == nil { // if student is not refer to any object create a student object
-		student = &Student{name: "ata"}
-	} else {
-		fmt.Println("Object already existed")
+		lock.Lock()
+		defer lock.Unlock()
+		// If two threads arrive lock.Lock() method, only one of them can enter inside
+		// Other one waits for first one. When first thread complete its job, second one enter inside
+		// This if condition blocks second thread to create same object again
+		if student == nil {
+			student = &Student{name: "ata"}
+		} else {
+			fmt.Println("Object already existed")
+		}
 	}
 	return student
 }
